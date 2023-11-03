d5d25c9eb78b0ca131c9571f29e586755719072e
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-11-03 17:25:19 +0900

    commit

--- 프로그래머스_단어 변환/main.swift
+++ 프로그래머스_단어 변환/main.swift
@@ -7,5 +7,55 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+    
+    if !words.contains(target) {
+        return 0
+    }
+    
+    // BFS
+    var visited = Array(repeating: false, count: words.count)
+    var queue: [(begin: String, cnt: Int)] = []
+    
+    queue.append((begin, 0))
+    
+    while (!queue.isEmpty) {
+        
+        let here = queue.removeFirst()
+        if here.begin == target {
+            return here.cnt
+        }
+        
+        for (idx, word) in words.enumerated() {
+            if !visited[idx] && isDifference(here.begin, word) {
+                queue.append((word, here.cnt + 1))
+                visited[idx] = true
+            }
+        }
+    }
+    
+    return 0
+}
 
+func isDifference(_ begin: String, _ next: String) -> Bool {
+    
+    let a = Array(begin)
+    let b = Array(next)
+    
+    var cnt = 0
+    
+    for i in 0..<a.count {
+        if a[i] != b[i] {
+            cnt += 1
+            if cnt > 1 {
+                return false
+            }
+        }
+    }
+    
+    if cnt == 1 {
+        return true
+    }
+    
+    return false
+}
